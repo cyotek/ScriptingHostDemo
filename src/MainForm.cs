4728b8cbcc3d616ba727b941f45aa0da6a42d83d
Richard Moss <richard.moss@cyotek.com>
2020-08-31 09:31:18 +0100

    Code headers

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -14,6 +14,17 @@ using System.Text;
 using System.Windows.Forms;
 using Program = Esprima.Ast.Program;
 
+// Adding Scripting to .NET Applications
+// https://www.cyotek.com/blog/adding-scripting-to-net-applications
+
+// Copyright © 2020 Cyotek Ltd. All Rights Reserved.
+
+// This work is licensed under the MIT License.
+// See LICENSE.TXT for the full text
+
+// Found this example useful?
+// https://www.paypal.me/cyotek
+
 namespace Cyotek.Demo
 {
   internal partial class MainForm : BaseForm
--- src/PixelPicture.cs
+++ src/PixelPicture.cs
@@ -1,6 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
 
+// Adding Scripting to .NET Applications
+// https://www.cyotek.com/blog/adding-scripting-to-net-applications
+
+// Copyright © 2020 Cyotek Ltd. All Rights Reserved.
+
+// This work is licensed under the MIT License.
+// See LICENSE.TXT for the full text
+
+// Found this example useful?
+// https://www.paypal.me/cyotek
+
 namespace Cyotek.Demo.ScriptingHost
 {
   internal class PixelPicture
