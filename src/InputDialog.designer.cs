0250852fdbf03458ff0165e38c89195ace3d0f18
Richard Moss <richard.moss@cyotek.com>
2020-11-02 12:08:52 +0000

    Fixed package paths

--- src/InputDialog.designer.cs
+++ src/InputDialog.designer.cs
@@ -76,6 +76,8 @@
       // InputDialog
       // 
       this.AcceptButton = this.okButton;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.CancelButton = this.cancelButton;
       this.ClientSize = new System.Drawing.Size(460, 97);
       this.Controls.Add(this.footerLabel);
